693d690cdebb42b6debd7a0720cd9e5efb5fc34d
하윤정 <112613300+YJeongs@users.noreply.github.com>
2024-10-10 03:22:18 +0900

    feature: seekBar 클릭시 재생 기능 구현

--- app/src/main/java/com/example/lieon/result/view/detail/ResultDetailViewModel.kt
+++ app/src/main/java/com/example/lieon/result/view/detail/ResultDetailViewModel.kt
@@ -1,29 +1,48 @@
 package com.example.lieon.result.view.detail
 
+import android.net.Uri
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.lieon.db.RecordHistoryEntity
 import com.example.lieon.db.RecordRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @HiltViewModel
 class ResultDetailViewModel @Inject constructor(
     private val recordRepository: RecordRepository
 ) : ViewModel() {
 
-    private var _selectedId : Int = 0
+    private var _selectedId: Int = 0
 
-    fun setSelectedId(id: Int){
+    private val _recordHistory = MutableLiveData<RecordHistoryEntity?>()
+    val recordHistory: LiveData<RecordHistoryEntity?> get() = _recordHistory
+
+    fun setSelectedId(id: Int) {
         _selectedId = id
+        fetchRecordHistoryById(id)
     }
 
-    suspend fun searchRecordHistoryById(id: Int): RecordHistoryEntity? {
-        return recordRepository.searchRecordHistoryById(id)
+    private fun fetchRecordHistoryById(id: Int) {
+        viewModelScope.launch {
+            val record = withContext(Dispatchers.IO) {
+                recordRepository.searchRecordHistoryById(id)
+            }
+            _recordHistory.value = record
+        }
     }
 
-    suspend fun updateRecordHistory(recordHistoryEntity: RecordHistoryEntity){
-        recordRepository.updateRecordHistory(recordHistoryEntity)
+    suspend fun updateRecordHistory(recordHistoryEntity: RecordHistoryEntity) {
+        withContext(Dispatchers.IO) {
+            recordRepository.updateRecordHistory(recordHistoryEntity)
+        }
+        _recordHistory.value = recordHistoryEntity
     }
 
     fun getSelectedId(): Int {
