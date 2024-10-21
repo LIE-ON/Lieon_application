31ee0f1da85c8db2936519ecb087cb269bc0b1a8
하윤정 <112613300+YJeongs@users.noreply.github.com>
2024-10-21 18:31:19 +0900

    fix: ui 개선

--- app/src/main/java/com/example/lieon/result/view/detail/ResultDetailFragment.kt
+++ app/src/main/java/com/example/lieon/result/view/detail/ResultDetailFragment.kt
@@ -1,9 +1,8 @@
 package com.example.lieon.result.view.detail
 
+import ResultDetailNameChangeDialog
 import android.media.MediaPlayer
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
--- app/src/main/java/com/example/lieon/result/view/detail/ResultDetailNameChangeDialog.kt
+++ app/src/main/java/com/example/lieon/result/view/detail/ResultDetailNameChangeDialog.kt
@@ -1,22 +1,17 @@
-package com.example.lieon.result.view.detail
-
-import android.app.Dialog
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.viewModels
+import com.example.lieon.R
 import com.example.lieon.databinding.DialogResultNameChangeBinding
 
 class ResultDetailNameChangeDialog(
-    private val onNameChanged: (newName:String)-> Unit
-): DialogFragment() {
+    private val onNameChanged: (newName: String) -> Unit
+) : DialogFragment() {
 
-    private var _binding : DialogResultNameChangeBinding? = null
-
+    private var _binding: DialogResultNameChangeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,22 +19,31 @@ class ResultDetailNameChangeDialog(
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = DialogResultNameChangeBinding.inflate(layoutInflater, container, false)
+        _binding = DialogResultNameChangeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.resultNameChangeSubmitButton.setOnClickListener{
+        binding.resultNameChangeSubmitButton.setOnClickListener {
             val newName = binding.resultNameChangeTitle.text.toString()
-            if(newName.isNotEmpty()){
+            if (newName.isNotEmpty()) {
                 onNameChanged(newName)
                 dismiss()
-            }else {
-                Toast.makeText(requireContext(),"이름 변경",Toast.LENGTH_SHORT).show()
-
+            } else {
+                Toast.makeText(requireContext(), "이름 변경", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setBackgroundDrawableResource(R.drawable.dialog_background)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
